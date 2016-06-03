dd725098e29b97611f44392f5590b89bf798c391
Misha <Misha@MISHA->
2016-06-03 15:18:07 +0300

    First Commit

---
+++ PunchCreator/PunchCreator/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PunchCreator.Startup))]
+namespace PunchCreator
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
